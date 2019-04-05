89c8011a4d36687b642f068a75509be8ae567107
oleggator <oleg@utkin.xyz>
2019-04-05 16:36:28 +0300

    Validate pk type

--- orm/orm/manager.py
+++ orm/orm/manager.py
@@ -24,13 +24,15 @@ class Manager:
         field_names = self.model_cls.get_field_names()
         field_names_string = ', '.join(field_names)
 
+        pk_validated = getattr(self.model_cls, pk_name).validate(pk)
+
         query = f'select {field_names_string} from {table_name} where {pk_name} = ?'
         cursor: Cursor = conn.cursor()
-        cursor.execute(query, (pk,))
+        cursor.execute(query, (pk_validated,))
 
         result = cursor.fetchone()
         if result is None:
-            raise DoesNotExistError(f'{self.model_cls.__name__} with {pk_name}={pk} does not exists')
+            raise DoesNotExistError(f'{self.model_cls.__name__} with {pk_name}={pk_validated} does not exists')
 
         field_values = dict(zip(field_names, result))
         return self.model_cls(**field_values)
