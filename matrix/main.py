5d0b57a5be4f97b168ca639dd69a049851d62617
Oleg Utkin <oleg_utkin@me.com>
2019-05-23 01:35:34 +0300

    Add matrix (#4)
    * mvp
    
    * Add list constructor
    
    * Class name fix
    
    * Add calloc
    
    * Handle empty list constructor error
    
    * incref on getitem
    
    * Fix indentation
    
    * fix transpose
    
    * Fix repr
    
    * fix transpose
    
    * Fix repr
    
    * fix matrices multiply
    
    * Add matrix python implementation
    
    * Add matrix division by integer
    
    * fix pymatrix repr and str

---
+++ matrix/main.py
@@ -0,0 +1,23 @@
+from matrix import Matrix as CMatrix
+from py_matrix import PyMatrix
+
+def test(matrix_class):
+    m = matrix_class([
+        [1, 2],
+        [3, 4],
+        [5, 6],
+    ])
+
+    n = 2 * m # multiply matrix by integer
+    n = n / 2 # divide matrix by integer
+    o = m + m # sum matrices
+    p = m.transpose() # transpose
+    q = m @ p # multiply matrices
+    r = 2 in m
+
+def main():
+    test(CMatrix)
+    test(PyMatrix)
+
+if __name__ == '__main__':
+    main()
